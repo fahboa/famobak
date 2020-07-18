323da0ba60caf9def0523505be15b83a66d721cf
fahboa <66919065+fahboa@users.noreply.github.com>
2020-07-18 01:09:35 +0100

    Add files via upload

---
+++ web development/js/script.js
@@ -0,0 +1,6 @@
+            // All animations will take exactly 500ms
+            var scroll = new SmoothScroll('a[href*="#"]', {
+                speed: 1000,
+                speedAsDuration: true
+                 });
+      
